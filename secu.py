a936d55e00f2ff7e4e373bf7b614ca4bc91e5df8
Unknown <unknown@example.com>
2020-09-10 20:06:18 +0200

    final???

--- secu.py
+++ secu.py
@@ -4,12 +4,27 @@ file = pyshark.FileCapture('03.pcap')
 dico = dict()
 # tuple (compteur,[ports],dernier_temps)
 for p in file:
-    #dico[p['ip'].src] = ()
+    var ip = p['ip'].src
+    var port = 0
     if hasattr(p,'icmp'):
-        print('icmp')
+        port = p.icmp.port
         pass
-    else if hasattr(p,'udp'):
-        pass
-    else if hasattr(p,'tcp'):
-        print ('tcp')
-        pass
+    elif hasattr(p,'udp'):
+        port = p.udp.port
+    elif hasattr(p,'tcp'):
+        port = p.tcp.port
+    else:
+        continue
+    if p['ip'].src not in dico.keys():
+        dico[p['ip'].src] = (0,[port],p.time)
+    else:
+        if port in dico[p['ip'].src][1]:
+            continue
+        if p.time - dico[p['ip'].src][2] < 0.01:
+            dico[p['ip'].src][1].append(port)
+            dico[p['ip'].src][0]+=1
+            if dico[p['ip'].src][0] == 50:
+                print("SCAN!!!!!")
+
+        
+    
