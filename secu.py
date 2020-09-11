d0074ef0d641abc76f2fd6e9b217f6898d322c7c
E-stein <eloise.stein@outlook.fr>
2020-09-11 22:46:23 +0200

    time problem

--- secu.py
+++ secu.py
@@ -4,27 +4,22 @@ file = pyshark.FileCapture('03.pcap')
 dico = dict()
 # tuple (compteur,[ports],dernier_temps)
 for p in file:
-    var ip = p['ip'].src
-    var port = 0
-    if hasattr(p,'icmp'):
-        port = p.icmp.port
-        pass
-    elif hasattr(p,'udp'):
+    print(p)
+    ip = p.ip.src
+    port = 0
+    if hasattr(p,'udp'):
         port = p.udp.port
     elif hasattr(p,'tcp'):
         port = p.tcp.port
     else:
         continue
-    if p['ip'].src not in dico.keys():
-        dico[p['ip'].src] = (0,[port],p.time)
+    if p.ip.src not in dico.keys():
+        dico[p.ip.src] = (0,[port], p.sniff_timestamp)
     else:
-        if port in dico[p['ip'].src][1]:
+        if port in dico[p.ip.src][1]:
             continue
-        if p.time - dico[p['ip'].src][2] < 0.01:
-            dico[p['ip'].src][1].append(port)
-            dico[p['ip'].src][0]+=1
-            if dico[p['ip'].src][0] == 50:
+        if float(p.sniff_timestamp) - float(dico[p.ip.src][2]) < 10:
+            dico[p.ip.src][1].append(port)
+            dico[p.ip.src][0]+=1
+            if dico[p.ip.src][0] == 50:
                 print("SCAN!!!!!")
-
-        
-    
