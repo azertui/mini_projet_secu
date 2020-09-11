e56c1b9bdd3154288cb353def1e18b67a55a27b8
azertui <rauch.arthur@gmail.com>
2020-09-11 23:26:30 +0200

    working scan

--- secu.py
+++ secu.py
@@ -1,10 +1,11 @@
 import pyshark
+from datetime import timedelta
 file = pyshark.FileCapture('03.pcap')
 
 dico = dict()
 # tuple (compteur,[ports],dernier_temps)
+delta = timedelta(milliseconds=10)
 for p in file:
-    print(p)
     ip = p.ip.src
     port = 0
     if hasattr(p,'udp'):
@@ -14,12 +15,13 @@ for p in file:
     else:
         continue
     if p.ip.src not in dico.keys():
-        dico[p.ip.src] = (0,[port], p.sniff_timestamp)
+        dico[p.ip.src] = [0,[port], p.sniff_time]
     else:
         if port in dico[p.ip.src][1]:
             continue
-        if float(p.sniff_timestamp) - float(dico[p.ip.src][2]) < 10:
+        if p.sniff_time - dico[p.ip.src][2] < delta:
             dico[p.ip.src][1].append(port)
             dico[p.ip.src][0]+=1
             if dico[p.ip.src][0] == 50:
-                print("SCAN!!!!!")
+                print("Warning! ",p.ip.src," is scanning you")
+        dico[p.ip.src][2]=p.sniff_time
